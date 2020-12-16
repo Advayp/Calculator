8b4d250fcc2c04fbc2eed52f02d30ab53f9a2697
Advay Patil <advaypatil27@gmail.com>
2020-12-16 13:17:26 -0800

    Added Comments and Improved Division Function

--- calc.py
+++ calc.py
@@ -1,26 +1,45 @@
+# Imports a module called math which has many interesting functions
+# relating to trigonometry, pi, and calculus
 import math
-import random
 
-intial_input = input("Enter the operation you want to do (add, subtract, multiply, divide, exponent, trig): ")
+# Assigns initial input to a random value so that the while loop on line 9
+# can run
+initial_input = 'asdfasdf'
+
+# Initializes a list which has the acceptable functions of this calculator
+acceptedValues = ['add', 'subtract', 'multiply', 'divide'] 
+
+# This while loop runs if the initial_input is not in acceptedValues.
+# Explanation of.lower() and .strip() functions on the first if statement in this calculator.
+while  not (initial_input.lower()).strip() in acceptedValues:
+
+    # Prompts the user for the function they want to enter
+    initial_input = input("Enter the operation you want to do (add, subtract, multiply, divide, exponent, trig): ")
+
+# Initializes 4 variables to a random value so that a while loop can run
 addNums = -1
+
 subtractNums = -1
+
 multiNums = -1
+
 divideNums = -1
 
 # Checks if the user typed in add
 # The.lower() function converts all characters in the string to lowercase
 # The .strip() function removes the spaces from the end and beginning from the string.
 # Eg: " HELLO " --> "hello". This transformation happens after the .lower() and .strip() functions
-if (intial_input.lower()).strip() == 'add':
+if (initial_input.lower()).strip() == 'add':
 
     # This while loop make sures the user types in a valid input
     while addNums < 2:
+        # This line prompts the user for input
         addNums = int(input("How many numbers would you like to add together? "))
 
     # This line intializes a variable called sum and sets it to 0
     sum = 0
 
-    # This for loop runs a certain number of times (Specifically: addNums number of times. See line 17)
+    # This for-loop runs a certain number of times (Specifically: addNums number of times. See line 17)
     for i in range(addNums):
 
         #This line takes an input from the user
@@ -34,34 +53,63 @@ if (intial_input.lower()).strip() == 'add':
 
 # Checks if user typed in 'subtract'
 # Uses same .lower() and .strip() functions
-if (intial_input.lower()).strip() == 'subtract':
+if (initial_input.lower()).strip() == 'subtract':
+
+    #{
+    # The lines of code below make sure that the
+    # number the user types in is valid for subtraction
+    # }
     while subtractNums < 2:
+        # This line of code prompts the user for input
         subtractNums = int(input("How many number(s) would you like to subtract? "))
+    
+    # Intializes a variable called 'difference' and sets it to 0
     differnce = 0
+
+    # Intializes a variable called 'difCalc' and sets it to 0
     difCalc = 0
+
+    # Prompts the user for input.
     placeDif = int(input("Enter the number you want to subtract " + str(subtractNums - 1) + " number(s) from: "))
 
+    # This for-loop runs 'subtractNums - 1' number of times.
     for i in range(subtractNums - 1):
+        
+        # Prompts the user for an integer that they want to subtract from 'placeDif'
         placeDif2 = int(input("Enter the number(s) you want to subtract from " + str(placeDif) + ": "))
+
+        # Adds the number the user entered for the previous prompt to a variable called 'difCalc'
         difCalc += placeDif2
-    differnce = placeDif - difCalc    
+    
+    #{
+    #  Takes the first number the user entered(placeDif) and subtracts the sum of
+    #  the numbers the user entered in the for-loop (difCalc)
+    # } 
+    differnce = placeDif - difCalc   
+
+    # Output 
     print("The final difference is " + str(differnce) + ".")
 
 
-if (intial_input.lower()).strip() == 'multiply':
+if (initial_input.lower()).strip() == 'multiply':
     while multiNums < 2:
         multiNums = int(input("How many numbers would you like to multiply together? "))
     firstMultnum = int(input("Enter the number you want to multiply " + str(multiNums) + " number(s) by: "))
     product = 1
 
-    for i in range(multiNums):
+    for i in range(multiNums - 1):
         multiBy = int(input("Enter the number(s) you want to multiply " + str(firstMultnum) + " by: "))
         product *= multiBy
     print("The product is " + str(product) + '.')
 
 
-if (intial_input.lower()).strip() == 'divide':
+if (initial_input.lower()).strip() == 'divide':
     while divideNums < 2:
         divideNums = int(input("How many numbers do you want to divide? "))
     quotient = int(input("Enter the dividend: "))
+
+    for i in range(divideNums - 1):
+        divisor = int(input("Enter divisor #" + str(i + 1) + ": "))
+        quotient /= divisor
+    print('The quotient is ' + str(quotient) + ".")
     
